35f0fd9fe35ede5ed7ee6b7b9c516cccad147b11
Amit Chapde <amitchapde2@gmail.com>
2025-11-27 23:18:18 +0530

    refactor:added fallback

--- logging-service/server.js
+++ logging-service/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const routes = require('./src/routes/logRoutes');
 const app = express();
 
-
 app.use(express.json());
 app.use('/log', routes);
 
 
-app.listen(3003, () => console.log('Logging Service running on port 3003'));
+const PORT = process.env.PORT || 3003;
+
+app.listen(PORT, () => {
+  console.log(`Logging Service running on port ${PORT}`);
+});
