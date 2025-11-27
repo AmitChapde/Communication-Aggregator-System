ae1362fad0548b4aa5e39d84b965a6637e3ef3c3
Amit Chapde <amitchapde2@gmail.com>
2025-11-27 23:37:17 +0530

    refact:deployment changes

--- delivery-service/server.js
+++ delivery-service/server.js
@@ -1,18 +1,6 @@
-const express = require('express');
-require('dotenv').config();
-
-const app = express();
-
-
-app.get('/health', (req, res) => res.send('OK'));
-
-s
 require('./src/consumers/emailConsumer');
 require('./src/consumers/smsConsumer');
 require('./src/consumers/whatsappConsumer');
 
 
-const PORT = process.env.PORT || 3002;
-app.listen(PORT, () => {
-  console.log(`Delivery Service Running on port ${PORT}`);
-});
+console.log("Delivery Service Running...");
